5a5badf4a161de0e187bea2400a82dcffe30962e
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2023-10-25 14:31:55 -0300

    Lista de códigos criados na aula do dia 25-out-23

--- scripts/Tank.gd
+++ scripts/Tank.gd
@@ -1,3 +1,4 @@
+tool
 extends KinematicBody2D
 
 var speed = 200 # Quantos pixels por segundo 
@@ -6,26 +7,58 @@ var speed = 200 # Quantos pixels por segundo
 var pre_bulllet = preload("res://scenes/Bullet.tscn")
 
 #lista de skins do tank 18/10 = 
-var tank_bodies = [
+var bodies = [
 	"res://sprites/tankBody_bigRed.png",
-	"res://sprites/tankBody_bigRed_outline.png",
 	"res://sprites/tankBody_blue.png",
-	"res://sprites/tankBody_blue_outline.png",
 	"res://sprites/tankBody_dark.png",
 	"res://sprites/tankBody_darkLarge.png",
-	"res://sprites/tankBody_darkLarge_outline.png"
+	"res://sprites/tankBody_green.png",
+	"res://sprites/tankBody_huge.png",
+	"res://sprites/tankBody_red.png",
+	"res://sprites/tankBody_sand.png"
 ]
+var barrels = [
+	"res://sprites/tankBlue_barrel1.png",
+	"res://sprites/tankBlue_barrel2.png",
+	"res://sprites/tankBlue_barrel3.png",
+	"res://sprites/tankDark_barrel1.png",
+	"res://sprites/tankDark_barrel2.png",
+	"res://sprites/tankDark_barrel3.png",
+	"res://sprites/tankGreen_barrel1.png",
+	"res://sprites/tankGreen_barrel2.png",
+	"res://sprites/tankGreen_barrel3.png",
+	"res://sprites/tankRed_barrel1.png",
+	"res://sprites/tankRed_barrel2.png",
+	"res://sprites/tankRed_barrel3.png",
+	"res://sprites/tankSand_barrel1.png",
+	"res://sprites/tankSand_barrel2.png",
+	"res://sprites/tankSand_barrel3.png"
+]
+export(int, 
+"Big Red", "blue","dark",
+"darkLarge","green","huge",
+"red","sand") var tankBody = 6 setget set_body
 
+export(int,
+"Blue 1", "Blue 2", "Blue 3",
+"Dark 1", "Dark 2", "Dark 3",
+"Green 1", "Green 2", "Green 3",
+"Red 1", "Red 2", "REd 3",
+"Sand 1", "Sand 2", "Sand 3") var barrelType = 2 setget set_barrel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#print(tank_bodies[0])
-	$sprite.texture = load(tank_bodies[2])
-	pass # Replace with function body.
+	pass
+	
 
-
+func _draw():
+	$sprite.texture = load(bodies[tankBody])
+	$Barrel/sprite.texture = load(barrels[barrelType])
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Engine.editor_hint:
+		return
+		
 	var dir_x = 0
 	var dir_y = 0
 	if Input.is_action_pressed("ui_right"):
@@ -51,5 +84,16 @@ func _process(delta):
 	translate(Vector2(dir_x, dir_y) * delta * speed)
 	# próxima aula a partir do video 15.11 
 	look_at(get_global_mouse_position())
+	
+func set_body(val):
+	tankBody = val
+	if Engine.editor_hint:
+		update()
+		
+func set_barrel(val):
+	barrelType = val
+	if Engine.editor_hint:
+		update()
+		
 
 		
