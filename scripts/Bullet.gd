15c2d6a80e5bc8e4a66d71f063afba1c82f5f7ca
Antonio Augusto Alves <antonioaugusto.alves1981@gmail.com>
2023-10-25 13:14:35 -0300

    Aula do dia 25-10

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var dir = Vector2(0, -1)
+var dir = Vector2(0, -1)setget set_dir #18.10
 var vel = 300 # px/s
 
 func _ready():
@@ -14,3 +14,9 @@ func _process(delta):
 func _on_Notifier_screen_exited():
 	print("Sai da tela")
 	queue_free()
+
+# Aula do dia 18.10	
+func set_dir(val):
+	print(val)
+	dir = val
+	rotation = dir.angle()
--- scripts/Tank.gd
+++ scripts/Tank.gd
@@ -4,8 +4,23 @@ var speed = 200 # Quantos pixels por segundo
 
 #Criando os disparos dinamicamente
 var pre_bulllet = preload("res://scenes/Bullet.tscn")
+
+#lista de skins do tank 18/10 = 
+var tank_bodies = [
+	"res://sprites/tankBody_bigRed.png",
+	"res://sprites/tankBody_bigRed_outline.png",
+	"res://sprites/tankBody_blue.png",
+	"res://sprites/tankBody_blue_outline.png",
+	"res://sprites/tankBody_dark.png",
+	"res://sprites/tankBody_darkLarge.png",
+	"res://sprites/tankBody_darkLarge_outline.png"
+]
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#print(tank_bodies[0])
+	$sprite.texture = load(tank_bodies[2])
 	pass # Replace with function body.
 
 
@@ -28,9 +43,13 @@ func _process(delta):
 		if counter < 6:
 			var bullet = pre_bulllet.instance()
 			bullet.global_position = $Barrel/Muzzle.global_position
+			#Aula do dia 18.10
+			bullet.dir = Vector2(cos(rotation), sin(rotation)).normalized()
 			get_parent().add_child(bullet)
-		
+			# 18.10
+			$Barrel/Animation.play("fire")
 	translate(Vector2(dir_x, dir_y) * delta * speed)
 	# próxima aula a partir do video 15.11 
+	look_at(get_global_mouse_position())
 
 		
